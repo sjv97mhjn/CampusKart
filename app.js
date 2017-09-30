1b131c7b39eb50acecad685b55b8dbe83733b0fa
thestormykid <hanugautam96@gmail.com>
2017-09-30 11:29:04 +0530

    Added proper folder structure

--- app.js
+++ app.js
@@ -4,39 +4,32 @@ var express 		= require('express'),
 	bcrypt			= require('bcrypt-nodejs'),
 	session			= require('express-session'),
 	mongoose		= require('mongoose'),
-	request			= require('request');
-var port = 3000;
+	request			= require('request'),
+	methodOverride  = require('method-override'),
 
-app.set('views',['./views']);
+	port 			= 3000,
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended :true}));
-app.use(express.static("public"));
-
-mongoose.connection.openUri("mongodb://sjv97mhjn:1997@ds151004.mlab.com:51004/campuskart");
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
-
-const commentchema = new mongoose.Schema({
-	name : String
-});
-
-const comment = mongoose.model('Comment',commentchema);
+	routes 			= require('./routes/route')
 
 
-app.get("/",function(req,res){
-	res.render("home.ejs");
-})
-app.post("/",function(req,res){
-	comment.create({name:req.body.comment},function(result,err){
-	if(err){
-		console.log(err);
-	}
-})
-	});
+app.use(bodyparser.urlencoded({ extended :true }));
+app.use(express.static(__dirname + "public"));
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+
+// if we use mongo in our localhost
+mongoose.connect("mongodb://localhost/campuskart");
+//  if we use mongolab
+// mongoose.connection.openUri("mongodb://sjv97mhjn:1997@ds151004.mlab.com:51004/campuskart");
+// mongoose.connection.on('error', (err) => {
+//   console.error(err);
+//   // console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+//   process.exit();
+// });
+
+app.use("/", routes);
+
 app.listen(port,function(){
-	console.log("Example App Listening On port" + port);
+	console.log("Listening On port " + port);
 })
